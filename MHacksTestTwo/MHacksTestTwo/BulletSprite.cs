d6c3155bdcd30cc73c87eeb1995b12bbad3dd6c8
gamer239 <gamer23170@gmail.com>
2014-01-19 06:21:41 -0500

    Updated bullet struct

--- MHacksTestTwo/MHacksTestTwo/BulletSprite.cs
+++ MHacksTestTwo/MHacksTestTwo/BulletSprite.cs
@@ -12,12 +12,13 @@ namespace MHacksTestOne
         {
             Vector2 velocity;
             Vector2 location;
+            Vector2 size;
         }
         public List<Bullet> bullet_arr;
 
         public BulletSprite()
         {
-
+            bullet_arr = new List<Bullet>();
         }
 
         public void spawn_bullet(Vector2 dir)
@@ -27,6 +28,12 @@ namespace MHacksTestOne
 
         public void Update()
         {
+            for (int i = 0; i < bullet_arr.Count; i++)
+            {
+                Bullet temp = bullet_arr.ElementAt(i);
+
+                //bullet_arr.
+            }
         }
 
         public void Draw()
